ac70e564435d54f137599cc3d6808b400cefe114
Andre Keys <andrekeys@u.boisestate.edu>
2023-04-05 15:19:15 -0600

    added calculator logic

---
+++ src/cs/calculator/src/ExecutorInterface.cs
@@ -0,0 +1,9 @@
+public interface ExecutorInterface 
+{
+    public void enterKey(Double value);
+    public Double plusKey(Double value);
+    public Double minusKey(Double value);
+    public Double multiplyKey(Double value);
+    public Double divideKey(Double value);
+    public void clearKey();
+}
---
+++ src/cs/calculator/src/RPNExecutor.cs
@@ -0,0 +1,36 @@
+public class RPNExecutor : ExecutorInterface 
+{
+    private Stack<Double> executionStack;
+
+    public RPNExecutor() {
+        executionStack = new Stack<double>();
+    }
+    public void enterKey(Double value) {
+        executionStack.Push(value);
+    }
+    public Double plusKey(Double value) {
+        double operand = popStack();
+        return operand + value;
+    }
+    public Double minusKey(Double value) {
+        double operand = popStack();
+        return operand - value;
+    }
+    public Double multiplyKey(Double value) {
+        double operand = popStack();
+        return operand * value;
+    }
+    public Double divideKey(Double value) {
+        double operand = popStack();
+        return operand / value;
+    }
+    public void clearKey() {
+        executionStack.Clear();
+    }
+
+    private Double popStack() {
+        return executionStack.Pop();
+    }
+
+    
+}
