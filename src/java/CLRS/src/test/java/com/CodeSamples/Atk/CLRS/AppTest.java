c03ed5fc2e4898b29e25d25ef04ed1caa55a2774
Andre Keys <andrekeys@u.boisestate.edu>
2023-04-24 21:03:34 -0600

    Adding project for working with intro to algorithms book

---
+++ src/java/CLRS/src/test/java/com/CodeSamples/Atk/CLRS/AppTest.java
@@ -0,0 +1,20 @@
+package com.CodeSamples.Atk.CLRS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
