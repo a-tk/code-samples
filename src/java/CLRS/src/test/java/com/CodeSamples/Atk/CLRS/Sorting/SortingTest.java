34cbfc081afda27c9254bb8bc42642518dc6c971
Andre Keys <andrekeys@u.boisestate.edu>
2023-05-30 11:19:21 -0600

    Add test class.

---
+++ src/java/CLRS/src/test/java/com/CodeSamples/Atk/CLRS/Sorting/SortingTest.java
@@ -0,0 +1,16 @@
+package com.CodeSamples.Atk.CLRS.Sorting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortingTest {
+
+    @Test
+    public void insertionSort() {
+    }
+
+    @Test
+    public void bubbleSort() {
+    }
+}
